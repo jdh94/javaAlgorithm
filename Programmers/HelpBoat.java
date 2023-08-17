91287c881e827d57d3d2e02ab16a04cf6a3e76b2
jdh94 <pnewlife@naver.com>
2023-08-17 23:16:47 +0900

    8.17 programmer test

---
+++ Programmers/HelpBoat.java
@@ -0,0 +1,23 @@
+package Programmers;
+
+import java.util.*;
+
+public class HelpBoat {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        Arrays.sort(people);
+        int index = 0;
+
+        for (int i = people.length - 1; i >= index; i--) {
+            if (people[i] + people[index] <= limit) {
+                index++;
+                answer++;
+            }
+            else {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+}
